2edd4adbf175181ae538a3f8b9726eefc5ac1086
mrcatman <kittenizator@yandex.ru>
2017-05-14 00:39:15 +0700

    Added a primitive index page

--- views/index.php
+++ views/index.php
@@ -1,2 +1,8 @@
 <h1>Виселица</h1>
+<? if (!isset($_SESSION['userid'])) { ?> 
 <a href="/login" class="btn">Войти</a>
+<a href="/register" class="btn">Зарегистрироваться</a>
+<? } else { ?> 
+<p class="greeting">Добро пожаловать, <strong><?=$_SESSION['username']?></strong></p>
+<a href="/game" class="btn">Начать игру</a>
+<? } ?>
